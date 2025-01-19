2e43fc5537120a4f2fe445377eef3799f4173b6b
logicallaw <logicallawbio@gmail.com>
2025-01-19 19:25:48 +0900

    feat: add a text in main_screen

--- team3/lib/screens/main_screen.dart
+++ team3/lib/screens/main_screen.dart
@@ -20,27 +20,43 @@ class MainScreen extends StatelessWidget {
         iconTheme: const IconThemeData(color: Colors.black),
       ),
       backgroundColor: Colors.white,
-      body: Center(
-        child: ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.white, // 버튼 배경색
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(16.0), // 둥근 모서리
+      body: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Text(
+              "Welcome, Admin", // 상단에 추가된 텍스트
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
             ),
-            fixedSize: const Size(100, 100), // 버튼 크기 (너비, 높이)
           ),
-          child: const Icon(
-            Icons.map, // 카메라 아이콘
-            size: 40, // 아이콘 크기
-            color: Colors.black, // 아이콘 색상
+          Center(
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white, // 버튼 배경색
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16.0), // 둥근 모서리
+                ),
+                fixedSize: const Size(100, 100), // 버튼 크기 (너비, 높이)
+              ),
+              child: const Icon(
+                Icons.map, // 지도 아이콘
+                size: 50, // 아이콘 크기
+                color: Colors.black, // 아이콘 색상
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const ForestMap()),
+                );
+              },
+            ),
           ),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const ForestMap())
-            );
-          },
-        ),
+          const SizedBox(height: 16), // 하단 버튼과 여백 추가
+        ],
       ),
     );
   }
